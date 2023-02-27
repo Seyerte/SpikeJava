31f1e487f83c0ee0d97f9a34274df8649f038935
Seyerte <estherseyer@gmail.com>
2023-02-27 16:15:21 +0100

    hello,bye,operadores,tipos

---
+++ src/com/SpikeJava/Words.java
@@ -0,0 +1,34 @@
+package com.SpikeJava;
+
+/**
+ * clase principal punto de entrada aplicacion
+ *
+ * @author Esther
+ */
+
+
+public class Words {
+
+    /**
+     *
+     * Funcion principal punto de entrada a la aplicacion
+     * @param args // argumentos a recibir
+     * @since 1.0
+     */
+    public static void main(String[] args) {
+        // write your code here
+        /*aqui un texto de comentario*/
+
+        /*
+
+        1.Abrir fichero
+        2.Leer fichero
+        3.Procesar fichero
+        4.Cerrar fichero
+
+         */
+
+        // imprimir texto
+        System.out.println("Hello world!"); // imprimir texto
+    }
+}
