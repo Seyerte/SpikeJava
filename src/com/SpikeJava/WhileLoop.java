bd0f26d5c8afab55da912f4f57762c1ccf86269f
Seyerte <estherseyer@gmail.com>
2023-02-27 18:14:36 +0100

    Bucles While

---
+++ src/com/SpikeJava/WhileLoop.java
@@ -0,0 +1,17 @@
+package com.SpikeJava;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        boolean check = true;
+
+        int count = 0;
+        while(count < 10){
+
+            System.out.println("Hola Mundo " + count);
+            count++;// condicion
+
+        }
+        System.out.println("Fin");
+    }
+}
