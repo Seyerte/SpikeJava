7e643fe1ade193c0477cff6ba4fa72527aa2c348
Seyerte <estherseyer@gmail.com>
2023-02-27 16:43:43 +0100

    Funciones

---
+++ src/com/SpikeJava/Funciones.java
@@ -0,0 +1,29 @@
+package com.SpikeJava;
+
+public class Funciones {
+    public static void main(String[] args) {
+
+        //holaMundo();
+        //holaMundo();
+
+        //holaMundo("Esther");
+        //holaMundo("Pepa");
+
+        String hola = devolverHola();
+        System.out.println(hola);
+
+    }
+    private static void holaMundo(){
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+    }
+    private static void holaMundo(String name){
+        System.out.println("Hola "+ name);
+    }
+
+    private static String devolverHola(){
+        return "Hola";
+    }
+
+
+}
