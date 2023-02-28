cfc91de54e5e6c49bb18cb1fd64921653cd16f79
Seyerte <estherseyer@gmail.com>
2023-02-28 12:02:33 +0100

    Try catch finally

---
+++ src/com/SpikeJava/TryCatchMain.java
@@ -0,0 +1,18 @@
+package com.SpikeJava;
+
+public class TryCatchMain {
+    public static void main(String[] args) {
+
+
+        try {
+            int result = 5 / 5;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Cierre de recursos");
+        }
+
+
+        System.out.println("fin");
+    }
+}
