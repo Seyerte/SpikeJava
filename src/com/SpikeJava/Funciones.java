13ce840f444620cd4fe1da2c6cbece9572255510
Seyerte <estherseyer@gmail.com>
2023-02-28 16:02:22 +0100

    crear e invocar una funcion

--- src/com/SpikeJava/Funciones.java
+++ src/com/SpikeJava/Funciones.java
@@ -3,9 +3,32 @@ package com.SpikeJava;
 public class Funciones {
     public static void main(String[] args) {
 
-        holaMundo();
-        holaMundo(4);
-        holaMundo("Nombre");
+        //Crear funciones
+
+        // opcion 1: funcion sin parametros y sin tipo retorno
+        //showMenu();
+        //showMenu();
+
+        // opcion 2: funcion sin parametros y con tipo retorno
+        String menu = getMenu();
+        System.out.println(menu);
+
+    }
+    static void showMenu() {
+        System.out.println("Bienvenido al E-commerce de zapatillas");
+        System.out.println("1- Ver zapatillas");
+        System.out.println("2- Comprar zapatillas");
+        System.out.println("3- Salir");
+    }
+
+    static String getMenu() {
+        return "Bienvenido al E-commerce de zapatillas: \n 1- Ver zapatillas...";
+    }
+
+
+       // holaMundo();
+       // holaMundo(4);
+        // holaMundo("Nombre");
         //holaMundo();
 
         //holaMundo("Esther");
@@ -14,7 +37,7 @@ public class Funciones {
         //String hola = devolverHola();
         //System.out.println(hola);
 
-    }
+
     public static void holaMundo(){
         System.out.println("Hola Mundo");
         System.out.println("Hola Mundo");
