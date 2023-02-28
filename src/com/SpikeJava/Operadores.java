391a5ee9ba14eb9fb116d9c99ef5ab56afd871bc
Seyerte <estherseyer@gmail.com>
2023-02-28 15:08:56 +0100

    Operadores aritmeticos

--- src/com/SpikeJava/Operadores.java
+++ src/com/SpikeJava/Operadores.java
@@ -3,18 +3,34 @@ package com.SpikeJava;
 public class Operadores {
     public static void main(String[] args) {
 
+        //ariteticos:
+        int numero1 = 10;
+        int numero2 = 20;
+        int resultadosuma = numero1 + numero2;
+
+        System.out.println(resultaadosuma);
+        System.out.println(resultadosuma + 5.77);
+
+        int resultadoResta = numero1 - numero2;
+
+        // comparacion:
+
+
+
+
+
 
         // Operadores
 
         // aritmeticos
 
         // + - / * %
-        int number1 = 1;
+        /*int number1 = 1;
         int number2 = 2;
         int resultado1 = number1 + number2;
         int resultado2 = number1 - number2;
         int resultado3 = number1 * number2;
-        int resultado4 = number1 / number2;
+        int resultado4 = number1 / number2;*/
 
         // logicos, relacion, comparacion, booleanos
 
