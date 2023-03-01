8c7a730a8f847d30c5ceeb4064dfc5bbcfb141f2
Seyerte <estherseyer@gmail.com>
2023-03-01 12:08:00 +0100

    if

---
+++ src/com/estructurasControl/condicionales/If.java
@@ -0,0 +1,16 @@
+package com.estructurasControl.condicionales;
+
+public class If {
+    public static void main(String[] args) {
+
+        int edad = 17;
+        boolean esMayor = edad >= 18;
+
+        if (esMayor) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+
+    }
+}
