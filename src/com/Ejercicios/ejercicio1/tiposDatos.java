a1effd830b04dc8e1a8df13f7c1f8faf59a48bed
Seyerte <estherseyer@gmail.com>
2023-03-01 16:47:50 +0100

    ejercicio 1

---
+++ src/com/Ejercicios/ejercicio1/tiposDatos.java
@@ -0,0 +1,23 @@
+package com.Ejercicios.ejercicio1;
+
+public class tiposDatos {
+    public static void main(String[] args) {
+
+        // tipos de datos
+
+        int numero1 = 10;
+        System.out.println(numero1);
+
+        long numero2 = 80L;
+        System.out.println(numero2);
+
+        double decimal1 = 9.5;
+        System.out.println(decimal1);
+
+        boolean verdadero = true;
+        System.out.println(verdadero);
+
+        String texto = "Soy un texto";
+        System.out.println(texto);
+    }
+}
