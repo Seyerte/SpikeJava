6ba1693a112b5a552b00de39a58283f961be560d
Seyerte <estherseyer@gmail.com>
2023-03-02 12:41:14 +0100

    Tecnica de consolidate conditional

--- src/com/Refactorizar/bad/MainBad.java
+++ src/com/Refactorizar/bad/MainBad.java
@@ -1,5 +1,22 @@
 package com.Refactorizar.bad;
 
 public class MainBad {
+    public static void main(String[] args) {
 
+      /*  double calculateExtraSalary {
+            double result = 0;
+            if (seniority<1) {
+                return 0;
+            }
+            if (education<1) {
+                return 0;
+            }
+            if (incidents<10) {
+                return 0;
+            }
+            if (!certificacion) {
+                return 0;
+            }
+        }*/
+    }
 }
---
+++ src/com/Refactorizar/good/MainGood.java
@@ -0,0 +1,19 @@
+package com.Refactorizar.good;
+
+public class MainGood {
+
+   /* double calculateExtraSalary(){ //Se comprime en un metodo
+        double result = 0;
+
+        if(!isEligibleExtraSalary()){
+            resturn result;
+        }
+
+// si es candidato entonces:
+//Calculamos el salario extra:
+
+// ...
+        return result;
+
+    }*/
+}
